0fa7d217f3d99cb64fe69a4ba9212878d65aaa28
yuanpeng <43242776+yuanpeng50@users.noreply.github.com>
2018-11-20 16:48:20 +0800

    Update train_eval_image_classifier.py

--- research/slim/train_eval_image_classifier.py
+++ research/slim/train_eval_image_classifier.py
@@ -18,15 +18,15 @@ def parse_args(check=True):
     #parser.add_argument('--checkpoint_exclude_scopes', type=str, default='densenet/logits,InceptionV4/AuxLogits/Aux_logits')
     #parser.add_argument('--checkpoint_exclude_scopes', type=str, default='densenet/logits')
     parser.add_argument('--train_dir', type=str)
-    parser.add_argument('--learning_rate', type=float, default=0.2)
+    parser.add_argument('--learning_rate', type=float, default=0.01)
     parser.add_argument('--clone_on_cpu', type=bool, default=False)
     #parser.add_argument('--clone_on_cpu', type=bool, default=True)
     parser.add_argument('--optimizer', type=str, default='rmsprop')
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=128)
 
     # eval
-    parser.add_argument('--dataset_split_name', type=str, default='train')
-    parser.add_argument('--eval_dir', type=str, default='validation')
+    parser.add_argument('--dataset_split_name', type=str, default='validation')
+    parser.add_argument('--eval_dir', type=str, default='/output/eval')
     parser.add_argument('--max_num_batches', type=int, default=100)
 
     FLAGS, unparsed = parser.parse_known_args()
